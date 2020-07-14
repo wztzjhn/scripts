36a111b885046ab9d353ee8754a29ed7db6caea5
Zhentao Wang <wztzjhn@gmail.com>
2020-07-14 12:10:42 -0400

    data_sort with python3 now.

--- data_sort_descending.py
+++ data_sort_descending.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 import numpy as np
 num_argv = len(sys.argv)
 
 print_blank = 0
-print '---------- mergesort data in descending order ----------'
+print('---------- mergesort data in descending order ----------')
 if ( num_argv<3 ):
     sys.exit("Usage: data_sort filename key1 [key2] ...")
 elif ( sys.argv[1]=='--help' ):
@@ -13,14 +13,14 @@ else:
     filename=sys.argv[1]
     keys=sys.argv[2:]
     num_keys=len(keys)
-    print "keys used for sorting: ",keys
+    print("keys used for sorting: ",keys)
 
 readfile = open(filename, "r")
 data = [line.split() for line in readfile]
 readfile.close()
 
 if ( data[0][0][0]!='#' ):
-    print 'data[0][0]:\t', data[0][0]
+    print('data[0][0]:\t', data[0][0])
     sys.exit("Check if your 1st line starts with #?")
 
 row_total = len(data)
@@ -31,11 +31,7 @@ col_end   = len(data[row_begin])-1
 col_keys  = []
 for key in keys:
     col_keys.append(data[row_begin-1].index(key))
-print 'col_keys-1:\t\t',col_keys
-#if data[row_begin-1].count('ERROR(TBRMV)')>0:
-#    col_error=data[row_begin-1].index('ERROR(TBRMV)')
-#else:
-#    col_error=-1
+print('col_keys-1:\t\t',col_keys)
 
 # function used to check if the argument string can be converted into float
 def is_number(s):
@@ -44,6 +40,7 @@ def is_number(s):
         return True
     except ValueError:
         return False
+
 # remove comment lines and blank lines
 maxlen = len(data[1])
 for row in range(row_end,row_begin-1,-1):
@@ -53,9 +50,7 @@ for row in range(row_end,row_begin-1,-1):
         del data[row]
     elif ( data[row][0][0]=='#' or not(is_number(data[row][col_keys[0]])) or not(is_number(data[row][col_keys[num_keys-1]])) ):
         del data[row]
-#    elif ( col_error!=-1 and float(data[row][col_error])>1.0 ):
-#        print "removing line with large errorbar in file "+filename+", error="+data[row][col_error]
-#        del data[row]
+
 row_total = len(data)
 row_begin = 2
 row_end   = row_total-1
@@ -88,8 +83,7 @@ for row in range(row_begin,row_end+1):
         val.append('')
     data_tbs.append(tuple(val))
 data_tbs = np.array(data_tbs, dtype=dt)
-data_tbs = data_tbs[np.argsort(data_tbs[keys], kind='mergesort')]
-
+index_argsort = np.argsort(data_tbs[keys], kind='mergesort')                   # sort
 
 filename = filename+".Dsorted"
 writefile = open(filename, "w")
@@ -97,16 +91,14 @@ for row in range(0,2):
     for word in data[row]:
         writefile.write(word + "\t")
     writefile.write("\n")
-flag = data_tbs[0][col_keys[0]]
-for term in data_tbs:
+flag = data[row_begin+index_argsort[0]][col_keys[0]]
+for i in range(0,len(index_argsort)):
+    term = data[row_begin+index_argsort[i]]
     if (term[col_keys[0]] != flag and print_blank == 1):
         writefile.write("\n")
-    for col in range(0,len(term)):
-        if col in col_keys:
-            writefile.write(str(-term[col])+"\t")                              # revert sign
-        else:
-            writefile.write(str(term[col])+"\t")
+    for word in term:
+        writefile.write(word + "\t")
     writefile.write("\n")
     flag = term[col_keys[0]]
 writefile.close()
-print '--------------------------------------------------------'
+print('--------------------------------------------------------')
